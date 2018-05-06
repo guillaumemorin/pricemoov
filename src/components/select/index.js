bec9b6cabfc0164b95ef30d0fc9b8f8714ef1cb3
Guillaume Morin <gmorin.pro@gmail.com>
2018-05-06 21:20:36 +0200

    Add value prop to Select component to make it a fully controlled component

--- src/components/select/index.js
+++ src/components/select/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-const Select = ({options = [], disabled, onChange, placeholder}) => {
+const Select = ({options = [], disabled, onChange, placeholder, value}) => {
     if(!Array.isArray(options)) return null;
     return (
         <div className="custom-select">
             <select disabled={disabled} onChange={onChange}>
-                <option value="" disabled selected>{placeholder}</option>
+                <option value="" disabled selected={!value}>{placeholder}</option>
                 {options.map((opt, index) => (
-                    <option key={index} value={opt.id}>{opt.name}</option>
+                    <option key={index} value={opt.id} selected={opt.id === value}>{opt.name}</option>
                 ))}
             </select>
         </div>
@@ -20,7 +20,8 @@ Select.propTypes = {
     options: PropTypes.array,
     disabled: PropTypes.bool,
     onChange: PropTypes.func,
-    placeholder: PropTypes.string
+    placeholder: PropTypes.string,
+    value: PropTypes.string
 };
 Select.defaultProps = {
     options: [],
