37f69fb9243522409927d666af49eaa385ff9285
Guillaume Morin <gmorin.pro@gmail.com>
2018-05-06 21:03:51 +0200

    Add main page app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import actions from './actions';
+import {Select, Toggle} from './components';
+import moment from 'moment';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    constructor(props){
+        super(props);
+        this.handleChangeAgencies = (event) => this.changeAgencies(event);
+        this.handleChangeCategories = (event) => this.changeCategories(event);
+        this.handleChangeValidated = (event) => this.changeValidated(event);
+        this.state = {
+            checkValidation: false,
+            prices: [],
+            agency: null,
+            agencies: [],
+            categories: []
+        }
+    }
+    componentWillMount(){
+        actions.agency.list()
+            .then((agencies) => this.setState({agencies}))
+    }
+    componentDidCatch() {
+        this.setState({hasError: true});
+    }
+    changeAgencies(event) {
+        const agency = event.target.value;
+        this.setState({agency});
+        actions.category.list(agency)
+            .then((categories) => this.setState({categories}));
+    }
+    changeCategories(event) {
+        const {agency} = this.state;
+        const category = event.target.value;
+        actions.price.list(agency, category)
+            .then((prices) => this.setState({prices}))
+    }
+    changeValidated(event) {
+        this.setState({checkValidation: event.target.checked});
+    }
+    render() {
+        const {checkValidation, agencies, categories, hasError, agency, prices} = this.state;
+        if(hasError) return <div>{'Something went wrong...'}</div>;
+        console.log('>>>>', checkValidation)
+
+        return (
+            <div>
+                <div className="header">
+                    <Select options={agencies}
+                            placeholder={'Select your agency'}
+                            onChange={this.handleChangeAgencies} />
+                    <Select options={categories}
+                            placeholder={'Select your category'}
+                            disabled={Boolean(!agency)}
+                            onChange={this.handleChangeCategories} />
+                </div>
+                <div className="results-container">
+                    {prices.length > 0 && <div className="results-container-row">
+                        <span className="results-container-cell">Date</span>
+                        <span className="results-container-cell">Price</span>
+                        <span className="results-container-cell">Suggested price</span>
+                    </div>}
+                    {prices.map((priceData, index) => {
+                        if(checkValidation && !priceData.isValidated) return null;
+                        return (
+                            <div className="results-container-row" key={index}>
+                                <span className="results-container-cell">{moment(priceData.startDate).format("YYYY-MM-DD HH:mm:ss")}</span>
+                                <span className="results-container-cell">{priceData.price}</span>
+                                <span className="results-container-cell">{priceData.suggestedPrice}</span>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="toggle-container">
+                    <span className="toggle-label">{'Validated'}</span>
+                    <Toggle onChange={this.handleChangeValidated}/>
+                </div>
+            </div>
+        );
   }
 }
 
