eacee189a1f879f6968fb4e661cbbf46abd640d9
Guillaume Morin <gmorin.pro@gmail.com>
2018-05-12 11:30:15 +0200

    Fix linting errors

--- src/actions.js
+++ src/actions.js
@@ -1,22 +1,24 @@
+/* eslint-disable no-undef */
+
 import {get} from './fetcher';
 
 export default {
     agency: {
-        list: () => new Promise(
-            (resolve) => get('agencies')
-                .then((res) => resolve(res))
-        )
+        list: () =>
+            new Promise(resolve => get('agencies').then(res => resolve(res)))
     },
     category: {
-        list: (agency) => new Promise(
-            (resolve) => get(`agencies/${agency}/categories`)
-                .then((res) => resolve(res))
-        )
+        list: agency =>
+            new Promise(resolve =>
+                get(`agencies/${agency}/categories`).then(res => resolve(res))
+            )
     },
     price: {
-        list: (agency, category) => new Promise(
-            (resolve) => get(`agencies/${agency}/categories/${category}/prices`)
-                .then((res) => resolve(res))
-        )
+        list: (agency, category) =>
+            new Promise(resolve =>
+                get(`agencies/${agency}/categories/${category}/prices`).then(
+                    res => resolve(res)
+                )
+            )
     }
 };
